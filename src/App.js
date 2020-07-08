f90daa5ac694c68477ab598ece036faaa0497dbf
Alan Murphy <alan.murphy@eiratech.com>
2020-07-08 13:58:12 +0100

    commit 3

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World
         </p>
         <a
           className="App-link"
