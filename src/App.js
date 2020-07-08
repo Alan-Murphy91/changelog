1a8d761974512ef1e17604d3e4f3d17dd5520baa
Alan Murphy <alan.murphy@eiratech.com>
2020-07-08 13:58:12 +0100

    commit 3

--- src/App.js
+++ src/App.js
@@ -8,10 +8,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello world!
         </p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World
+        </p>
+        <p>
+          Hello Worlds
         </p>
         <a
           className="App-link"
